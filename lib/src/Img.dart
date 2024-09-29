6d561b8b9a836c0d3c2b4ee104f85ef8b0cc7373
irajeshh <85046336+irajeshh@users.noreply.github.com>
2024-09-29 14:27:50 +0530

    responsive img setup implemented

--- lib/src/Img.dart
+++ lib/src/Img.dart
@@ -1,9 +1,11 @@
 // ignore_for_file: public_member_api_docs, unsafe_html
 
 import 'dart:async';
+import 'dart:convert' as convert;
 
 // import 'package:cached_network_image/cached_network_image.dart';
 import 'package:cached_network_image/cached_network_image.dart';
+import 'package:extensions/extensions.dart';
 import 'package:flutter/material.dart';
 import 'package:universal_html/html.dart' as html;
 
@@ -70,14 +72,13 @@ class _ImgState extends State<Img> with AutomaticKeepAliveClientMixin, ImgViewer
   @override
   Widget build(final BuildContext context) {
     super.build(context);
-    final BorderRadius borderRadius = widget.shape ?? BorderRadius.circular(widget.radius ?? 8);
     return Container(
       height: widget.height,
       width: widget.width,
       padding: EdgeInsets.all(widget.outterPadding ?? 0),
       child: Material(
         elevation: widget.elevation ?? 1.5,
-        borderRadius: borderRadius,
+        borderRadius: widget.shape ?? BorderRadius.circular(widget.radius ?? 8),
         clipBehavior: Clip.antiAlias,
         color: widget.imgColor ?? Colors.white,
         child: imgWidget(),
@@ -86,10 +87,10 @@ class _ImgState extends State<Img> with AutomaticKeepAliveClientMixin, ImgViewer
   }
 
   Widget? imgWidget() {
-    final Widget imgWidget = widget.imgUrl == null
+    final Widget imgWidget = _imgUrl == null
         ? errorWidget()
         : CachedNetworkImage(
-            imageUrl: widget.imgUrl!,
+            imageUrl: _imgUrl!,
             fit: widget.fit ?? BoxFit.fill,
             color: widget.blendColor,
             colorBlendMode: widget.blendMode,
@@ -168,21 +169,73 @@ class _ImgState extends State<Img> with AutomaticKeepAliveClientMixin, ImgViewer
 
   ///If onClicked is given, then call it alone!
   Future<void> onTap() async {
-    debugPrint(widget.imgUrl);
+    debugPrint(_imgUrl);
     if (onClicked != null) {
       html.window.open(onClicked!, '_blank');
     } else if (widget.onTap != null) {
       widget.onTap!();
     } else {
-      await showImage(context, widget.imgUrl);
+      await showImage(context, _imgUrl);
     }
   }
 
   Future<void> clearCache() async {
-    if (widget.clearCache && widget.imgUrl != null) {
-      // await CachedNetworkImage.evictFromCache(widget.imgUrl!);
+    if (widget.clearCache && _imgUrl != null) {
+      // await CachedNetworkImage.evictFromCache(_imgUrl!);
     }
   }
 
-  String? get onClicked => widget.imgUrl?.split('onClicked=').lastOrNull;
+  String? get _imgUrl {
+    String? u = widget.imgUrl;
+    if (u != null) {
+      ///Check if url is a JSON
+      if (u.isJSON) {
+        printt('$u is a JSON');
+
+        ///Check the device type
+        DeviceType deviceType = DeviceType.laptop;
+        final double screenWidth = MediaQuery.of(context).size.width;
+        final double screenHeight = MediaQuery.of(context).size.height;
+        final double aspectRatio = screenWidth / screenHeight;
+        if (aspectRatio < 0.74) {
+          deviceType = DeviceType.mobile;
+        } else if (aspectRatio >= 0.74 && aspectRatio < 1.0) {
+          deviceType = DeviceType.tablet;
+        }
+        printt('deviceType is ${deviceType.name}');
+
+        ///Try to parse the json and retrive the respective URL
+        try {
+          printt('Trying to convert json from u');
+          final Object? j = convert.jsonDecode(u);
+          if (j is Json) {
+            printt('j is Json');
+            final String? mobile = j.nullableString(DeviceType.mobile.name);
+            final String? tablet = j.nullableString(DeviceType.tablet.name);
+            final String? laptop = j.nullableString(DeviceType.laptop.name);
+            switch (deviceType) {
+              case DeviceType.mobile:
+                u = mobile ?? tablet ?? laptop;
+              case DeviceType.laptop:
+                u = laptop ?? tablet ?? mobile;
+              case DeviceType.tablet:
+                u = tablet ?? (aspectRatio >= 1.0 ? laptop : mobile);
+            }
+            printt('mobile: $mobile');
+            printt('tablet: $tablet');
+            printt('laptop: $laptop');
+          } else {
+            printt('j is not Json');
+          }
+        } on Exception catch (e) {
+          printt('Error parsing JSON from ($u) :Error $e');
+        }
+      } else {
+        printt('$u is not a JSON');
+      }
+    }
+    return u?.replaceAll(' ', '');
+  }
+
+  String? get onClicked => _imgUrl?.split('onClicked=').lastOrNull;
 }
